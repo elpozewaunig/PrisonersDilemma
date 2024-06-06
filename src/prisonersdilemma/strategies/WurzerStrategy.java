adada8432d45bd1cac00ea66c9888105f46e214f
towurzer <t.wurzer.2004@gmail.com>
2024-06-06 10:51:39 +0200

    lets just use reflection to avoid detection

--- src/prisonersdilemma/strategies/WurzerStrategy.java
+++ src/prisonersdilemma/strategies/WurzerStrategy.java
@@ -5,132 +5,175 @@ import prisonersdilemma.GameState;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class WurzerStrategy implements GameStrategy {
+public class WurzerStrategy implements GameStrategy{
   private final int testOpponentXTimes = 10;
   private int roundCounter = 0;
   private int finalRoundCounter = 0;
 
   private int tournamentRound = 0;
 
+  private GameStrategy[] opponents = new GameStrategy[testOpponentXTimes];
+  private GameStrategy[] opponentsToReset = new GameStrategy[testOpponentXTimes];
+
   @Override
   public String getName() {
     return "\033[35;49;1m[Q1] CheatingWurzel\033[0m";
   }
 
-  /**
-   * Tries to cheat in different ways
-   * First of all, wouldn't consider that cheating, but we are counting rounds and if it's the last round we auto defect
-   * Second of all we try to cheat: we give the opponent the game state and look how it reacts x times.
-   * If it defects every time, we defect to, if it cooperates, we cooperate to;
-   * If it's inconsistent, we try to reset the thing by giving it an empty game state. If the reaction is consistent we react accordingly
-   * If not the opponent is seen as random and we auto defect
-   * If the cheating doesn't work, we play titForTat
-   */
   @Override
   public GameAction playRound(GameState state) {
     roundCounter++; // new round
-    var otherPlayerActions = state.player1() == this ? state.player2Actions() : state.player1Actions();
     var otherPlayer = (state.player1() == this) ? state.player2() : state.player1(); // get opponent
+    var otherPlayerActions = state.player1() == this ? state.player2Actions() : state.player1Actions();
+
+    //----------------------------------------------------------------------------------------------------
+    if (otherPlayer.getClass() == this.getClass()) // play against self; cooperate // instance of didn't work
+      return GameAction.COOPERATE;
 
     if (otherPlayerActions.isEmpty()) { // new tournament Round
       roundCounter = 1; // reset roundCounter
       tournamentRound++; // new tournamentRound
+      resetOpponentArray(otherPlayer);
     }
-
-
-    // If last round - defect
     else if (tournamentRound > 1 && roundCounter == finalRoundCounter)
+      // If last round - defect
       return GameAction.DEFECT;
-    else if(tournamentRound < 2) // first tournament round - figure out how many rounds are played, in order to defect at the last one
+    else if(tournamentRound < 2)
+      // first tournament round - figure out how many rounds are played, in order to defect at the last one
       finalRoundCounter = roundCounter;
 
-    // not the last round
+    // else: play against opponent, and it's not the last round
 
-    try { // try to cheat
+    try{ // try to cheat...
+      // ...with letting the instances in the opponent array play and predict the action of the opponent by the action of its clones
+      // using clones, so that it's harder to detect the cheating try (i.e. Nuclear Gandis anti cheat method)
+      // test reaction using current game state
+      GameAction reaction = testOpponentArray(state);
+      if (reaction == GameAction.COOPERATE) // opponent cooperated cooperate
+        return GameAction.COOPERATE;
 
-      // test reactions of opponent from current game state #cheatingWurzel
-        GameAction reaction = testOpponent(otherPlayer, state);
-        if (reaction != null) // reaction was consistent return ideal reaction
-          return reaction;
+      // opponent didn't cooperate consistently, try forcing him with sending him an empty game state and therefore
+      // possibly resetting his internal variables
+      GameAction resetReaction = resetAndTestOpponentArray(state);  // test reactions of opponent when resetting
+      // reaction was consistently a Cooperation, cooperate
+      if (resetReaction == GameAction.COOPERATE) {
+        resetArrayBecauseOfAction(state); // reset array of running clones, to be able to make accurate predictions
+        otherPlayer.playRound(emptyGameState(state)); // reset other player
+        return GameAction.COOPERATE; // cooperate
+      }
 
-        // else: try resetting and test reactions of opponent afterward (works if opponent resets internal variables when simulating a new round)
-        GameAction resetReaction = testOpponentWithReset(otherPlayer, state);  // test reactions of opponent when resetting
-        // reaction was consistent return ideal reaction ||| else: inconsistent reactions -> probably random strat -> defect
-        return Objects.requireNonNullElse(resetReaction, GameAction.DEFECT);
+      // reaction was either inconsistent or a consistent deflection, deflect
+      return GameAction.DEFECT;
 
-    } catch (Exception e) { // cheating didn't work, play titForTat
-      otherPlayer.playRound(new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>())));
-        return playTitForTat(otherPlayerActions);
+    } catch (Exception e) { // cheating didn't work, play TFT
+      return playTitForTat(otherPlayerActions);
     }
   }
 
+  /**
+   * Play TFT
+   */
+  private GameAction playTitForTat(List<GameAction> otherPlayerActions){
+    if (otherPlayerActions.isEmpty()) {
+      return GameAction.COOPERATE;
+    }
+    return otherPlayerActions.getLast() == GameAction.COOPERATE ? GameAction.COOPERATE : GameAction.DEFECT;
+  }
 
 
+  /**
+   * create instances of opponent and save them in the opponents array in order to maybe get an advantage by trying
+   * to disclose the reaction of the opponent beforhand
+   */
+  private void resetOpponentArray(GameStrategy otherPlayer){
+    try { // try filling opponent Array up with instances of current opponent
+      for (int i = 0; i < opponents.length; i++){
+        opponents[i] = (GameStrategy) otherPlayer.getClass().getConstructors()[0].newInstance();
+        opponentsToReset[i] = (GameStrategy) otherPlayer.getClass().getConstructors()[0].newInstance();
+      }
+    } catch (java.lang.InstantiationException | java.lang.IllegalAccessException | java.lang.reflect.InvocationTargetException e){
+      // something went wrong, fill the array up with null
+      opponents = new GameStrategy[opponents.length];
+      opponentsToReset = new GameStrategy[opponents.length];
+    }
+  }
 
-
-  private GameAction testOpponent(GameStrategy otherPlayer, GameState state){
-    // get reactions
-    GameAction[] reactions = new GameAction[testOpponentXTimes];
-    for (int i = 0; i < testOpponentXTimes; i++)
-      reactions[i] = otherPlayer.playRound(state);
-
+  /**
+   * evaluate wheter opponents responses where consistent (if not he probably plays a random strategy)
+   */
+  private boolean reactionsAreConsistent(GameAction[] reactions){
     // evaluate reactions (should work with reactions.stream...) but it's late and I don't want to think ;)
     GameAction reaction0 = reactions[0];
-    boolean consistent = true;
     for (int i = 1; i < testOpponentXTimes; i++){
       if (!reactions[i].equals(reaction0)){
-        consistent = false;
-        break;
+        return false;
       }
     }
+    return true;
+  }
 
+  /**
+   * return the best GameAction in response to the reactions of the opponent to the current game state
+   */
+  private GameAction returnBestGameAction(GameAction[] reactions){
+    // evaluate reactions
+    boolean consistent = reactionsAreConsistent(reactions);
 
     // return reaction
-    if (consistent && reaction0.equals(GameAction.COOPERATE)) // if all reactions cooperate: cooperate
+    if (consistent && reactions[0].equals(GameAction.COOPERATE)) // if all reactions cooperate: cooperate
       return GameAction.COOPERATE;
     else if (consistent) // if all reactions defect: defect
       return GameAction.DEFECT;
     return null;
   }
 
-
-  private GameAction testOpponentWithReset(GameStrategy otherPlayer, GameState state){
-    GameAction[] reactions = new GameAction[10];
-    GameAction reaction0 = reactions[0];
-    // reset and get reactions
-    for (int i = 0; i < testOpponentXTimes; i++) {
-      otherPlayer.playRound(new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>())));
-      reactions[i] = otherPlayer.playRound(state);
+  /**
+   * let the opponent Instances in the opponents array play with the current game state and evaluate the best response
+   */
+  private GameAction testOpponentArray(GameState state){
+    // get reactions
+    GameAction[] reactions = new GameAction[testOpponentXTimes];
+    for (int i = 0; i < opponents.length; i++) {
+      reactions[i] = opponents[i].playRound(state);
+      opponentsToReset[i].playRound(state);
     }
 
-
-    // evaluate reactions (should work with reactions.stream...) but it's late and I don't want to think ;)
-    boolean consistent = true;
-    for (int i = 1; i < testOpponentXTimes; i++){
-      if (!reactions[i].equals(reaction0)){
-        consistent = false;
-        break;
-      }
-    }
-
-    // reset and return reaction
-    if (consistent && reaction0.equals(GameAction.COOPERATE)) {// if all reactions cooperate: reset and cooperate
-      otherPlayer.playRound(new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>())));
-      return GameAction.COOPERATE;
-    } else if (consistent) { // if all reactions defect: reset and defect
-      otherPlayer.playRound(new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>())));
-      return GameAction.DEFECT;
-    }
-    otherPlayer.playRound(new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>())));
-    return null;
+    return returnBestGameAction(reactions);
   }
 
-  private GameAction playTitForTat(List<GameAction> otherPlayerActions){
-    if (otherPlayerActions.isEmpty()) {
-      return GameAction.COOPERATE;
+
+  /**
+   * Reset the oppenent instances in the opponentsToReset Array and evaluate reactions
+   */
+  private GameAction resetAndTestOpponentArray(GameState state) {
+    // get reactions
+    GameAction[] reactions = new GameAction[testOpponentXTimes];
+    GameState emptyState = emptyGameState(state);
+    for (int i = 0; i < opponents.length; i++) {
+      opponentsToReset[i].playRound(emptyGameState(emptyState));
+      reactions[i] = opponentsToReset[i].playRound(state);
     }
-    return otherPlayerActions.getLast() == GameAction.COOPERATE ? GameAction.DEFECT : GameAction.COOPERATE;
+
+    return returnBestGameAction(reactions);
+  }
+
+  /**
+   * when reseting there was a consistent cooperation, so we will reset the Main Opponent instance
+   * to be able to make accurate preditions in the future, we will also reset all the clones that should be on the
+   * same state as the main opponent
+   */
+  private void resetArrayBecauseOfAction(GameState state){
+    GameState emptyState = emptyGameState(state);
+    for (GameStrategy opponent : opponents)
+      opponent.playRound(emptyGameState(emptyState));
+  }
+
+
+  /**
+   * create an empty game state
+   */
+  private GameState emptyGameState(GameState state){
+    return new GameState(state.player1(), state.player2(), new ArrayList<>(new ArrayList<>()), new ArrayList<>(new ArrayList<>()));
   }
 }
